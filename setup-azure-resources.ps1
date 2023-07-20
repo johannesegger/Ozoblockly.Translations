14f85760cf88bb87d7f80973ae5b442ed7b0e58c
Johannes Egger <j.egger@posteo.at>
2023-07-20 07:32:50 +0200

    Include setting up Container App in setup script

--- setup-azure-resources.ps1
+++ setup-azure-resources.ps1
@@ -1,10 +1,27 @@
+$RepositoryName = gh repo view --json nameWithOwner --jq .nameWithOwner
 $SubscriptionId = az account show --query id -o tsv
-$ResourceGroup = "rg-ozoblockly-editor"
+$ResourceLocation = "westeurope"
+$ResourceGroupName = "rg-ozoblockly-translations"
+$ContainerAppName = "ca-ozoblockly-translations"
+$ContainerAppEnvironmentName = "cae-ozoblockly-translations"
+
+$ResourceGroup = az group create --name $ResourceGroupName --location $ResourceLocation ` | ConvertFrom-Json
+$ContainerAppEnvironment = az containerapp env create --name $ContainerAppEnvironmentName --resource-group $ResourceGroupName `
+    --location $ResourceLocation `
+    --logs-destination none
+    | ConvertFrom-Json
+$ContainerApp = az containerapp create --name $ContainerAppName --resource-group $ResourceGroupName `
+    --environment $ContainerAppEnvironmentName `
+    --ingress external --target-port 80 `
+    --cpu 0.25 --memory 0.5Gi `
+    --min-replicas 0 --max-replicas 1
+    | ConvertFrom-Json
+
 $ServicePrincipal = az ad sp create-for-rbac `
     --name "ozoblockly-translations" `
     --role contributor `
-    --scopes /subscriptions/$SubscriptionId/resourceGroups/$ResourceGroup `
+    --scopes /subscriptions/$SubscriptionId/resourceGroups/$ResourceGroupName `
     --sdk-auth
     | ConvertFrom-Json
-$RepositoryName = gh repo view --json nameWithOwner --jq .nameWithOwner
+
 gh secret set AZURE_CREDENTIALS -a actions -b (ConvertTo-Json $ServicePrincipal) --repo $RepositoryName
